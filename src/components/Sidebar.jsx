9795ecf645b142e698004c1e60552c90bf6fb65a
Gogo602 <gogomarosi61@gmail.com>
2025-04-11 02:42:09 +0300

    updated dashboard

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -24,7 +24,7 @@ const Sidebar = () => {
     return (
         <div className="bg-gray-100 text-gray-900 h-screen px-4 fixed w-16 md:w-48 border-r border-gray-300 dark:border-gray-600 dark:bg-gray-900 dark:text-white">
             <h1 className='text-2xl font-bold hidden md:block mt-4 text-center italic'>
-                <img src="/logo.png" alt="" />
+               Gogo602
             </h1>
             <ul className='flex flex-col mt-5 text-sm font-bold'>
 				<Link to='/dashboard' className={`flex items-center py-3 px-2 space-x-4 hover:rounded hover:cursor-pointer ${isActive('/dashboard') ? activeClass : inactiveClass}`}>
